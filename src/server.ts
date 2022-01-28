499e6746bc665dc44117ba06acbee9664cdebef6
Luis Pato <lpato@dfau.de>
2022-01-28 14:18:06 +0100

    add correct path to the alien avatar images

--- src/server.ts
+++ src/server.ts
@@ -6,11 +6,13 @@ const PORT = 8000;
 
 const app = express();
 
+app.use(express.static('alien_avatars'));
+
 /**
  * Routes for the aliens
  */
 app.use('/api/aliens', apiRouter);
 
 app.listen(PORT, () => {
-	console.log(`⚡️ Random Aliens running on http://localhost:${PORT}`);
+	console.log(`⚡️ Random Aliens server running on http://localhost:${PORT}`);
 });
